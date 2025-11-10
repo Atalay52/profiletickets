0d8581ca02303b3969ced4b73b847f20bfe8871f
Atalay52 <148344381+Atalay52@users.noreply.github.com>
2025-11-10 12:49:06 +0300

    Create image.js

---
+++ api/image.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+  const svg = `&lt;svg width="600" height="400" xmlns="http://www.w3.org/2000/svg"&gt;
+    &lt;rect width="600" height="400" fill="#4F46E5"/&gt;
+    &lt;text x="300" y="200" font-family="Arial" font-size="24" fill="white" text-anchor="middle"&gt;
+      Ticket Trade - Ready!
+    &lt;/text&gt;
+  &lt;/svg&gt;`;
+  res.setHeader('Content-Type', 'image/svg+xml');
+  res.send(svg);
+}
